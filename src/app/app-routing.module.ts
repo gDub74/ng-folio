dd472c07c54438fc9692b1a4adeadf6705a1f25a
Greg Weber <gregoryweberguitars@gmail.com>
2018-05-16 10:48:06 -0700

    first commit - nav and parent routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { WeatherComponent } from './home/weather/weather.component';
+import { TeamComponent } from './home/team/team.component';
+import { MoviesComponent } from './home/movies/movies.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'weather',
+    component: WeatherComponent
+    // add children routes for weather app
+  },
+  {
+    path: 'team',
+    component: TeamComponent
+    // add team app children routes
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent
+    // add movie children
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './home/weather/weather.component';
+import { TeamComponent } from './home/team/team.component';
+import { NavComponent } from './nav/nav.component';
+import { MoviesComponent } from './home/movies/movies.component';
 
+import { MovieService } from './services/movie.service';
+import { TeamService } from './services/team.service';
+import { WeatherService } from './services/weather.service';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    TeamComponent,
+    NavComponent,
+    MoviesComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [MovieService, TeamService, WeatherService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
